1a9457a5070fea3d81bc9f22c5bfa5735d57df6d
entrar <entrar@PC-1195>
2018-04-17 11:37:14 +0100

    WS	opcional v2.0

--- src/trabajodecursocliente/TrabajoDeCursoCliente.java
+++ src/trabajodecursocliente/TrabajoDeCursoCliente.java
@@ -6,6 +6,7 @@
 package trabajodecursocliente;
 
 import com.google.gson.Gson;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -13,13 +14,16 @@ import org.json.JSONObject;
  * @author entrar
  */
 public class TrabajoDeCursoCliente {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {             
+        mostrarDatosTodos();
+        System.out.println("-----Prueba con Datos Individuales-------");
+        System.out.println("-----------------------------------------");
         mostrarDatos("1");
         mostrarDatos("2");
     }
     public static void mostrarDatos(String id){
-        BaseDeDatos sergio = new BaseDeDatos();      
-        String res = sergio.buscarPorID(String.class,id);
+        BaseDeDatos BDD = new BaseDeDatos();      
+        String res = BDD.buscarPorID(String.class,id);
         JSONObject obj = new JSONObject(res);
         String r1 = obj.toString();
         Gson gson = new Gson();
@@ -30,6 +34,25 @@ public class TrabajoDeCursoCliente {
         System.out.println("dept: "+emp.getDept());
         System.out.println("sueldo: "+emp.getSueldo());
         System.out.println("-----------------------------------------");
+    }
+    
+    public static void mostrarDatosTodos(){
+        BaseDeDatos BDD = new BaseDeDatos();      
+        String res = BDD.MostrarTodosLosEmpleados(String.class);    
+        JSONArray arr = new JSONArray(res);
+        for(int i=0; i<arr.length();i++){
+        JSONObject obj = arr.getJSONObject(i);
+        String r1 = obj.toString();
+        Gson gson = new Gson();        
+        Empleado emp = gson.fromJson(r1, Empleado.class);
+        System.out.println("id: "+emp.getId());
+        System.out.println("Nombre: "+emp.getNombre());
+        System.out.println("Contraseña: "+emp.getContraseña());
+        System.out.println("dept: "+emp.getDept());
+        System.out.println("sueldo: "+emp.getSueldo());
+        System.out.println("-----------------------------------------");
+        }
+        
     }
     
 }
